83b83cb250157a979d30f7b61a872d01751bc769
arm3nta <arma2002@hotmail.com>
2026-01-14 12:34:49 -0600

    Update main.py

--- main.py
+++ main.py
@@ -29,6 +29,11 @@ def set_background(png_file):
         bin_str = get_base64(png_file)
         page_bg_img = f'''
         <style>
+        .stMarkdown {
+    background-color: rgba(255, 255, 255, 0.8);
+    padding: 10px;
+    border-radius: 5px;
+}
         .stApp {{
             background-image: url("data:image/png;base64,{bin_str}");
             background-size: cover;
